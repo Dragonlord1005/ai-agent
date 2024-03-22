5df63ee60f98a226c4589b0b9d8b913da5e7ca71
Dragonlord1005 <dragonlord100587@gmail.com>
2024-03-22 10:41:45 -0600

    Add main.py with API key import

---
+++ main.py
@@ -0,0 +1,12 @@
+# We need to import the api key from the .env
+from dotenv import load_dotenv
+import os
+from e2b import Sandbox
+
+load_dotenv()
+
+# Import the API key
+API_KEY = os.getenv("E2B_API_KEY")  
+
+sandbox = Sandbox(API_KEY)
+# sandbox.close()
